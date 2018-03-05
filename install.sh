f71d23ff1af4eab41f84e0ae2fddbe90fd9085ca
ivanpeso <37067291+ivanpeso@users.noreply.github.com>
2018-03-05 16:26:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-# bash <(curl -Ss )
-# 
+# bash <(curl -Ss https://raw.githubusercontent.com/ivanpeso/test/master/install.sh)
+#
 mkdir ~/nxPanel/
 wget https://raw.githubusercontent.com/ivanpeso/test/master/base/start.sh -O ~/nxPanel/start.sh
+cd ~/nxPanel/
+echo "Just execute ./start.sh install"
