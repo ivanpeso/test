0eab466c3fc4372f213079f01ed9e6b3a201fb46
ivanpeso <37067291+ivanpeso@users.noreply.github.com>
2018-03-05 16:28:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 #
 mkdir ~/nxPanel/
 wget https://raw.githubusercontent.com/ivanpeso/test/master/base/start.sh -O ~/nxPanel/start.sh
+chmod +x ~/nxPanel -R
 echo "Files copied in your home dir! Just go to ~/nxPanel/ "
 echo "and execute ./start.sh install"
