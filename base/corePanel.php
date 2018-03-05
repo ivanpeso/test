3bee3994268a900184435a425e8f0196791dc6a4
ivanpeso <37067291+ivanpeso@users.noreply.github.com>
2018-03-05 16:47:12 +0100

    Update corePanel.php

--- base/corePanel.php
+++ base/corePanel.php
@@ -1,7 +1,7 @@
 <?php
 //Output PID file immidietly
 shell_exec("echo ".getmypid().">p.id");
-if ($_SERVER['REMOTE_ADDR'] != trim(file_get_contents(nxPanel/myip)), " ")  exit("nevalja!");
+if ($_SERVER['REMOTE_ADDR'] != trim(file_get_contents(nxPanel/myip)," "))exit("nevalja!");
 echo "ovo radi!!!fgsdfgsdf";
 
 //echo $_SERVER['REMOTE_ADDR'];
